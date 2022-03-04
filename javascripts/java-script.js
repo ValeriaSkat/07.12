27c555b0bbe7b87b1a488e67c3d50d7894354fcf
ValeriaSkat <lera.vw@mail.ru>
2022-03-04 23:37:08 +0300

    отлот

---
+++ javascripts/java-script.js
@@ -0,0 +1,58 @@
+$(document).ready(function(){
+$('.imgone').hover(function(){
+$('.imgone').attr('src', 'img/imgthree.png');
+},function(){
+$('.imgone').attr('src', 'img/imgtwo.png');
+})
+})
+$(document).ready(function(){
+$('.img9').hover(function(){
+$('.img9').attr('src', 'img/img10.png');
+},function(){
+$('.img9').attr('src', 'img/img9.png');
+})
+})
+$(document).ready(function(){
+$('.img11').hover(function(){
+$('.img11').attr('src', 'img/img12.png');
+},function(){
+$('.img11').attr('src', 'img/img11.png');
+})
+})
+$(document).ready(function(){
+$('.img13').hover(function(){
+$('.img13').attr('src', 'img/img14.png');
+},function(){
+$('.img13').attr('src', 'img/img13.png');
+})
+})
+$(document).ready(function(){
+$('.img15').hover(function(){
+$('.img15').attr('src', 'img/img16.png');
+},function(){
+$('.img15').attr('src', 'img/img15.png');
+})
+})
+$(document).ready(function(){
+$('.img177').click(function(){
+$('.img177').attr('src', 'img/img18.png');
+})
+$('.img177').click(function(){
+$('.img177').attr('src', 'img/img18.png');
+})
+})
+$(document).ready(function(){
+$('.img199').click(function(){
+$('.img199').attr('src', 'img/img20.png');
+})
+$('.img199').click(function(){
+$('.img199').attr('src', 'img/img20.png');
+})
+})
+$(document).ready(function(){
+$('.img211').hover(function(){
+$('.img211').attr('src', 'img/img22.png');
+},function(){
+$('.img211').attr('src', 'img/img21.png');
+})
+})
